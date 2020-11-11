b0ce9a2779e8f3303472d9fce62fe6e50fafd276
dolgopolovdc <dolgopolovdc@gmail.com>
2020-11-11 14:17:53 +0300

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fabc74fb1c8af001eb50939",
+				"5fabc74fb1c8af001eb5093b"
+			]
+		},
+		"5fabc74fb1c8af001eb50939": {
+			"id": "5fabc74fb1c8af001eb50939",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fabc74fb1c8af001eb5093b": {
+			"id": "5fabc74fb1c8af001eb5093b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
